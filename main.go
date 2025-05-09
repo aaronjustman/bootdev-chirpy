207394c9b713e5a12c0ea3fb7b02f4caa35ee443
AaronJ <aaron.justman@gmail.com>
2025-05-09 15:11:58 -0700

    Step 2: Added the file server as the handler for the root of localhost.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	serve_mux := http.NewServeMux()
+	serve_mux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serve_mux,
