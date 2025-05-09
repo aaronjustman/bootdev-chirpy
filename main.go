6b22087c4b188d73418352ef3f2616adfaad6bc4
AaronJ <aaron.justman@gmail.com>
2025-05-09 16:13:19 -0700

    Added the /app handler.

--- main.go
+++ main.go
@@ -6,7 +6,17 @@ import (
 
 func main() {
 	serve_mux := http.NewServeMux()
-	serve_mux.Handle("/", http.FileServer(http.Dir(".")))
+	serve_mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serve_mux.HandleFunc("/healthz", func(rw http.ResponseWriter, req *http.Request) {
+		req.Header.Set("Content-Type", "text/plain; charset=utf-8")
+		rw.WriteHeader(200)
+
+		_, err := rw.Write([]byte("OK"))
+		if err != nil {
+			panic("the Write went wrong...")
+		}
+	})
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serve_mux,
